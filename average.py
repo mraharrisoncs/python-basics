2f49bb50526a263b402c340cd53b2ef8e10ad30b
Mr A Harrison <alanjharrison@bcs.org>
2017-08-08 16:25:54 +0100

    Add files via upload

---
+++ average.py
@@ -0,0 +1,13 @@
+#Program to calculate the Average - Mr Harrison
+
+
+num1=int(input("Enter num1? "))
+num2=int(input("Enter num2? "))
+
+
+total = num1 + num2
+average = total / 2
+
+
+print("The average of {0} and {1} is {2}".format(num1,num2,average))
+
